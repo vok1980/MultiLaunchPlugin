450254611acbb86b64d4952fe6248d531a9e5122
Vyacheslav Koshcheyev <vok1980@gmail.com>
2015-01-24 22:08:42 +0600

    add class for Launch tab

--- src/com/voksoft/multilauncherplugin/ui/LaunchConfigurationTabMain.java
+++ src/com/voksoft/multilauncherplugin/ui/LaunchConfigurationTabMain.java
@@ -1,5 +1,41 @@
 package com.voksoft.multilauncherplugin.ui;
 
-public class LaunchConfigurationTabMain {
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.swt.widgets.Composite;
+
+
+
+public class LaunchConfigurationTabMain extends AbstractLaunchConfigurationTab {
+
+	@Override
+	public void createControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		return "&Launches";
+	}
 
 }
