f9656881e6db4e689d96c041ccb2cd7533ebbe12
Vyacheslav Koshcheyev <vok1980@gmail.com>
2015-01-24 21:48:12 +0600

    add common tab through plugin.xml

--- src/com/voksoft/multilauncherplugin/ui/LaunchConfigurationTabGroup.java
+++ src/com/voksoft/multilauncherplugin/ui/LaunchConfigurationTabGroup.java
@@ -7,20 +7,9 @@ import org.eclipse.debug.ui.ILaunchConfigurationTab;
 public class LaunchConfigurationTabGroup extends
 		AbstractLaunchConfigurationTabGroup {
 	
-	private enum TabIdxs {
-		TI_COMMON,
-		
-		TI_COUNT,
-	};
-
 	public LaunchConfigurationTabGroup() {
-		ILaunchConfigurationTab[] tabs = getTabs();
-		assert( tabs == null );
 		
-		tabs = new ILaunchConfigurationTab[ TabIdxs.TI_COUNT.ordinal() ];
-		tabs[TabIdxs.TI_COMMON.ordinal()] = new org.eclipse.debug.ui.CommonTab();
-		
-		setTabs(tabs);
+		setTabs( new ILaunchConfigurationTab[0] );
 	}
 
 	@Override
@@ -29,7 +18,6 @@ public class LaunchConfigurationTabGroup extends
 		ILaunchConfigurationTab[] tabs = getTabs();
 		assert( tabs != null );
 		
-		dialog.setActiveTab( tabs[ TabIdxs.TI_COMMON.ordinal() ] );		
 
 	}
 
